7beaf019d0865dcbb2c00ef7a50a8414d2ba14e0
ciaranshaw <ciaranshaw@ua-eduroam-ten-25-143-41.uniaccess.unimelb.edu.au>
2016-07-31 04:50:42 +1000

    Updated

--- public/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', ['ngRoute', 'myApp.home','myApp.searchpage', 'myApp.gig',])
-
-.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-	$locationProvider.hashPrefix('!');
-	$routeProvider.otherwise({redirectTo: '/'});
-}]);
